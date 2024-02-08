4d395381fe146a1088d1d0f57d63ad62e5c93805
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2024-02-08 11:59:25 +0100

    :pencil2: on macro, improve readibility

--- src/main/scala/blockly2scafi/macros/GeneratorsList.scala
+++ src/main/scala/blockly2scafi/macros/GeneratorsList.scala
@@ -1,10 +1,7 @@
 package blockly2scafi.macros
-import scala.util.{Try => Verify}
-import blockly2scafi.generators.{UnitBlockType, ValueBlockType}
 import scala.quoted.*
+import scala.util.Try as Verify
 object GeneratorsList:
-  //inline def unitBlocks(): Seq[UnitBlockType] = ${unitBlocksImplementation}
-  
   transparent inline def searchByType[T](inline path: String): List[T] =
     ${ searchByTypeMacro[T]('path) }
 
@@ -29,45 +26,18 @@ object GeneratorsList:
     val news = recursiveClassDef(symbol).filter(isClassEligible).map(
       clazz => Apply(
         Select(New(TypeIdent(clazz.symbol)), clazz.constructor.symbol),
-        List.empty)
+        List.empty
+      )
     )
 
     val expressions = news.map(_.asExprOf[T])
     Expr.ofList(news.map(_.asExprOf[T]))
   
-  def recursiveClassDef(using q: Quotes)(symbol: q.reflect.Symbol): List[q.reflect.ClassDef] = {
+  def recursiveClassDef(using q: Quotes)(symbol: q.reflect.Symbol): List[q.reflect.ClassDef] =
     import q.reflect.*
     symbol.declarations.collect {
       case obj if obj.isClassDef =>
-        val verified = Verify(obj.tree.asInstanceOf[ClassDef]) 
-        verified.map(List(_)).getOrElse(List.empty)
+        Verify(obj.tree.asInstanceOf[ClassDef]).map(List(_)).getOrElse(List.empty)
       case obj if obj.isPackageDef => recursiveClassDef(obj)
     }.flatten
-  }
-  def searchSymbolForDeclaration(using q: Quotes)(symbol: q.reflect.Symbol, test: q.reflect.ValDef => Boolean): Seq[q.reflect.ValDef] =
-    import q.reflect.*
-
-    try {
-      symbol.declarations.collect {
-        case obj if obj.fullName.contains("#") =>
-          Seq.empty
-        case pkg if pkg.isPackageDef =>
-          searchSymbolForDeclaration(pkg, test)
-        case obj if obj.isValDef &&
-          symbol.isValDef &&
-          !symbol.isPackageDef &&
-          obj.tree.asInstanceOf[ValDef].tpt.tpe =:= symbol.tree.asInstanceOf[ValDef].tpt.tpe =>
-          Seq.empty
-        case obj if obj.isValDef && test(obj.tree.asInstanceOf[ValDef]) =>
-          Seq(obj.tree.asInstanceOf[ValDef])
-        case obj if obj.isValDef =>
-          searchSymbolForDeclaration(obj, test)
-        case _ =>
-          Seq.empty
-      }.flatten
-    } catch {
-      case e: Throwable =>
-        Seq.empty
-    }
-
 
